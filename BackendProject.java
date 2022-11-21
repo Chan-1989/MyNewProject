9ed8b91db35925e8593575c314b9646ce23b1d2e
Chan-1989 <chanbasha.duvur@gmail.com>
2022-11-21 12:19:02 +0530

    modifed first line

--- BackendProject.java
+++ BackendProject.java
@@ -1,3 +1,3 @@
-This is backend project for developing the backend code.
+This is backend project for developing the backend code with "proper setup".
 This is important project code.
 This is Development code modifiedin "Development Branch"
